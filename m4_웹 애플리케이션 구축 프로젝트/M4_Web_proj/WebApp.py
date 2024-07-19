483e7c5d4a9c492e8bb76f2bad00c2618adb0bc0
boseon1 <168054433+boseon1@users.noreply.github.com>
2024-07-19 14:52:25 +0900

    Add files via upload

--- m4_웹 애플리케이션 구축 프로젝트/M4_Web_proj/WebApp.py
+++ m4_웹 애플리케이션 구축 프로젝트/M4_Web_proj/WebApp.py
@@ -35,7 +35,6 @@ def translate_keyword_g(keyword, target_language='en'):
     translator = Translator()
     keyword = keyword.replace(' ', '')
     translation = translator.translate(keyword, dest=target_language)
-    print('구글 translation.text  == ' + translation.text)
     return translation.text
 
 # deepl 번역
@@ -44,7 +43,6 @@ def translate_keyword_d(keyword):
     translator = deepl.Translator(auth_key)
     keyword = keyword.replace(' ', '')
     result = translator.translate_text(keyword, target_lang='EN-US')
-    print('deepl result.text  == ' + result.text)
     return result.text
 
 # 네이버 번역
@@ -59,7 +57,6 @@ def translate_keyword_n(text):
         translated_text = result['message']['result']['translatedText']
         if translated_text.lower().startswith('a '): translated_text = translated_text.replace('a ', '', 1)
         elif translated_text.lower().startswith('an '): translated_text = translated_text.replace('an ', '', 1)
-        print('네이버 translated_text  == ' + translated_text)
         return translated_text
     else:
         print(f"Error: {response.status_code}, {response.text}")
@@ -106,8 +103,22 @@ filtered_data_patents = pd.DataFrame()
 filtered_data_papers = pd.DataFrame()
 
 
+# categories = {
+#     '제너럴': ['인공 지능','인공지능','뉴럴 네트워크','chat gpt','챗 gpt','챗 지피티','제미나이','메타 라마','ILSVRC','이미지넷','이미지 넷','알렉스넷','알렉스 넷','추론','신경망','신경 망','학습','훈련방법','ai','지능형','훈련 방법','지도방법','지도 방법','학습방법','학습 방법','머신러닝','머신 러닝','딥러닝','딥 러닝','패턴인식','패턴 인식','블록체인','블록 체인','이미지인식','이미지 인식','비전','자연어 처리','자연어처리','챗봇','기계학습','기계 학습','심층학습','심층 학습','제스처 인식','제스쳐 인식','피사체 인식','객체 인식','오브젝트 인식','동작 인식','모션 인식','안면 인식','거대 언어','거대언어','생성형','일반 인공지능','일반 인공 지능','일반 ai','자율 인공지능','자율 ai','자율 학습'],
+#     '의료':['의료','항체','헬스','치료','항체','건강','유전자','병원','유도체','환자','동물','의료기기','의료 기기','의료정보','의료 정보','바이오의료','바이오 의료','의학','약물','수술','재활','임상','의사','간호','응급','진단','면역','질환','질병','치료제','보건','헬스케어','생체'],
+#     '전자상거래서비스': ['상거래','상품','주문','맞춤 서비스','클라이언트','배송','판매','결제','공급망','공급 망','판매망','판매 망','서비스','전자상거래플랫폼','전자 상거래 플랫폼','전자상거래 플랫폼','온라인쇼핑','온라인 쇼핑','온라인 거래','디지털결제','디지털 결제','온라인서비스','온라인 서비스','온라인 플랫폼','온라인플랫폼','인터넷 거래','인터넷거래','인터넷 서비스','인터넷서비스','고객','구매자','트래픽','방문자','페이지뷰','세션 시간','세션시간','이메일 구독','이메일구독','마케팅','광고','QR','연관 상품','연관상품','통관','관세','환율'],
+#     '자동차': ['자동차','주행','진입','변속기','자율주행','자율 주행','도로','차량','충돌','교통','비행','보행자','차선','신호등','전기차','하이브리드','내연기관','커넥티드카','커넥티드 카','스마트카','스마트 카','운전 보조 시스템','운전보조 시스템','운전보조시스템','ADAS','차량용센서','차량센서','차량 센서','차량용 센서','모빌리티','운송','운전','주차','헤드업 디스플레이','헤드업디스플레이','내비게이션'],
+#     '금융': ['금융','코인','트레이딩','투자','신용','자산','블록체인','블록 체인','디지털자산','디지털 자산','간편결제','자동결제','입금','출금','재정','은행','보험','경제','자산','부채','대출','이자','지불','결제','송금','환율','외환','주식','채권','증권','펀드','연금','퇴직금','저축','예금','지급 보증','핀테크','온라인 뱅킹','온라인뱅킹','인터넷 뱅킹','인터넷뱅킹','크라우드 펀딩','크라우드펀딩','크립토커런시','비트코인','알트코인','캐피털'],
+#     '교육': ['교육','강의','교육용','시험','성적','학생','학습관리','학습 관리','자기주도학습','학교','대학','강의','교수','교실','e러닝','이러닝','에듀테크','가상 교실','가상 수업','디지털 교과서','교사','교육자'],
+#     '농업': ['농업','농사','작황','작물','스마트농업','스마트 농업','농작물','농약','수확','스마트팜','품종'],
+#     '엔터테인먼트': ['콘텐츠','컨텐츠','증강 현실','증강현실','게임','미디어','사용자인터페이스','가상현실','음성인식','가상현실콘텐츠','엔터테인먼트','영화','드라마','애니메이션','뮤지컬','공연','콘서트','음악','음원','음반','뮤직비디오','뮤직 비디오','라디오','팟캐스트','팟 캐스트','오디오북','오디오 북','웹툰','웹소설','웹 소설','스트리밍','넷플릭스','디즈니','아마존','유튜브','트위치','틱톡','페이스북','인스타그램','전시회'],
+#     '보안': ['보안','침입','탐지','데이터보호','데이터 보호','위협예측','위협 예측','인증','암호화','암호','안전기술','위험','방화벽','바이러스','스파이','랜섬웨어','피싱','스팸','해독','디지털 서명','디지털서명','디지털 인증서','디지털인증서','토큰','보안 프로토콜','보안프로토콜','위협 방지','위협 분석','위협 대응','위협 완화','위협 모니터링','위협 방어','위협 평가','위협 예측','위협 방어','위협 차단','위협 관리','침해 탐지','침해 방지','침해 분석','침해 대응','침해 완화','침해 모니터링','침해 평가','침해 예측','침해 방어','침해 차단','침해 관리'],
+#     '자동화시스템': ['자동화','로봇자동화','로봇 자동화','제어시스템','제어 시스템','IoT','로봇제어','로봇 제어','스마트시스템','스마트 시스템','온라인서비스','온라인 서비스'],
+#     '반도체': ['뉴로모픽', '인공지능 반도체', 'ai 반도체', 'npu', '양자 소자','양자 컴퓨팅','양자 알고리즘']
+# } 
+
 categories = {
-    '제너럴': ['artificial intelligence', 'neural network', 'chat gpt', 'gemini', 'meta llama', 'ilsvrc', 'imagenet', 'alexnet', 'inference', 'learning', 'training methods', 'ai', 'intelligent', 'training method', 'supervised methods', 'learning methods', 'machine learning', 'deep learning', 'pattern recognition', 'blockchain', 'image recognition', 'vision', 'natural language processing', 'chatbot', 'gesture recognition', 'object recognition', 'motion recognition', 'facial recognition', 'large language models', 'generative', 'general ai', 'autonomous ai', 'autonomous learning'],
+    '제너럴': ['artificial intelligence', 'neural network', 'chat gpt', 'gemini','llama', 'meta llama', 'ilsvrc', 'imagenet', 'alexnet', 'inference', 'learning', 'training methods', 'ai', 'intelligent', 'training method', 'supervised methods', 'learning methods', 'machine learning', 'deep learning', 'pattern recognition', 'blockchain', 'image recognition', 'vision', 'natural language processing', 'chatbot', 'gesture recognition', 'object recognition', 'motion recognition', 'facial recognition', 'large language models', 'generative', 'general ai', 'autonomous ai', 'autonomous learning'],
     '의료': ['healthcare', 'antibody', 'health', 'treatment', 'wellness', 'gene', 'hospital', 'derivative', 'patient', 'animal', 'medical device', 'medical information', 'bio-medical', 'medicine', 'drug', 'surgery', 'rehabilitation', 'clinical', 'doctor', 'nurse', 'emergency', 'diagnosis', 'immune', 'disease', 'disorder', 'therapy', 'public health', 'biometrics'],
     '전자상거래서비스': ['commerce', 'product', 'order', 'customized services', 'client', 'delivery', 'sales', 'payment', 'supply chain', 'service', 'e-commerce platform', 'online shopping', 'online transaction', 'digital payment', 'online service', 'online platform', 'internet transaction', 'internet service', 'customer', 'buyer', 'traffic', 'visitor', 'page view', 'session time', 'email subscription', 'marketing', 'advertisement', 'qr', 'related products', 'customs clearance', 'tariff', 'exchange rate'],
     '자동차': ['automobile', 'driving', 'entry', 'transmission', 'autonomous driving', 'road', 'vehicle', 'collision', 'traffic', 'flight', 'pedestrian', 'lane', 'traffic light', 'electric vehicle', 'hybrid', 'internal combustion engine', 'connected car', 'smart car', 'driver assistance system', 'adas', 'vehicle sensor', 'mobility', 'transportation', 'parking', 'head-up display', 'navigation'],
@@ -144,7 +155,7 @@ def search():
         translated_keyword_d = translate_keyword_d(search_keyword)  ## Deepl Translation
         translated_keyword_n = translate_keyword_n(search_keyword)  ## Naver PAPAGO Translation
         # 3가지 번역된 키워드 리스트
-        translated_keywords = [translated_keyword_g.lower(), translated_keyword_d.lower(), translated_keyword_n.lower()]
+        translated_keywords = [translated_keyword_g.lower().replace(' ',''), translated_keyword_d.lower().replace(' ',''), translated_keyword_n.lower().replace(' ','')]
         print(f"Translated Keyword: {translated_keywords}")
 
         try:
@@ -177,24 +188,36 @@ def search():
             filtered_data_patents = filtered_df_patents
             print(f"Filtered patents data: {filtered_data_patents.shape}")
 
-            # if filtered_df_patents.empty:
-            #     print('ddd')
-            #     return render_template('Web.html', table="", plot="", top3_table="", top5_table="", paper_table="", date=date, form=form)
+
+            table_html = filtered_df_patents[['Application Number', 'Application Date', 'Applicant', 'Title', 'Status']]
+            table_html = table_html.rename(columns={
+                'Application Number' : '출원번호',
+                'Application Date' : '출원일',
+                'Applicant' : '출원인',
+                'Title' : '제목',
+                'Status' :'상태'
+            })
+            table_html = table_html.to_html(index=False, classes="table", escape=False)
+            print("Generated table HTML")
+
+            if filtered_df_patents.empty:
+                # return render_template('Web.html', table="", plot="", top3_table="", top5_table="", paper_table="", date=date, form=form)
+                table=""; plot=""; top3_table=""; top5_table=""; table_html = ""
 
             # 논문 데이터 필터링
             if filter_type == 'patent':
                 paper_conditions = []
                 for field in application_fields:
                     for keyword in categories.get(field, []):
-                        paper_conditions.append((df_papers['title'].str.lower().contains(keyword, na=False)) | 
-                                                (df_papers['Abstract'].str.lower().contains(keyword, na=False)))
+                        paper_conditions.append((df_papers['title'].str.lower().str.contains(keyword, na=False)) | 
+                                                (df_papers['Abstract'].str.lower().str.contains(keyword, na=False)))
                 if paper_conditions:
                     filtered_df_papers = df_papers[pd.concat(paper_conditions, axis=1).any(axis=1)]
                     # 필터 조건 생성
                     filter_condition = pd.Series([False] * len(filtered_df_papers), index=filtered_df_papers.index)
                     for translated_keyword in translated_keywords:
-                        filter_condition |= filtered_df_papers['title'].str.contains(translated_keyword, na=False) | \
-                                            filtered_df_papers['Abstract'].str.contains(translated_keyword, na=False)
+                        filter_condition |= filtered_df_papers['title'].str.lower().str.contains(translated_keyword, na=False) | \
+                                            filtered_df_papers['Abstract'].str.lower().str.contains(translated_keyword, na=False)
                     filtered_df_papers = filtered_df_papers[filter_condition]
                     filtered_data_papers = filtered_df_papers
                 else:
@@ -217,17 +240,6 @@ def search():
             else:
                 filtered_data_papers = pd.DataFrame()
                 paper_table_html = ""
-
-            table_html = filtered_df_patents[['Application Number', 'Application Date', 'Applicant', 'Title', 'Status']]
-            table_html = table_html.rename(columns={
-                'Application Number' : '출원번호',
-                'Application Date' : '출원일',
-                'Applicant' : '출원인',
-                'Title' : '제목',
-                'Status' :'상태'
-            })
-            table_html = table_html.to_html(index=False, classes="table", escape=False)
-            print("Generated table HTML")
 
             top3_df = filtered_df_patents['Applicant'].value_counts().head(3).reset_index()
             if len(top3_df) < 3:
